e1277c78e01b648c226bb6613cb477175debdb29
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-01-15 16:07:51 +0000

    Update script to latest syntax

--- app-setup-dev.ps1
+++ app-setup-dev.ps1
@@ -46,8 +46,8 @@ DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Feeds"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Data.ActiveDirectory"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Cms"
 DownloadProjectIfMissing $parentFolderOfThisScript "Escc.Elibrary"
-NuGetRestoreForProject "$parentFolderOfThisScript\Escc.Egms"
-NuGetRestoreForProject "$parentFolderOfThisScript\Escc.Cms"
+NuGetRestoreForProject $parentFolderOfThisScript "Escc.Egms"
+NuGetRestoreForProject $parentFolderOfThisScript "Escc.Cms"
 
 CheckSiteExistsBeforeAddingApplication("Escc.EastSussexGovUK")
 EnableDotNet40InIIS
