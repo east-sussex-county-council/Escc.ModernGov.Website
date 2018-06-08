b05fd2fef23f68627471acc08efed11be6821616
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-06-08 12:22:45 +0100

    Convert project from WebForms to MVC

---
+++ Escc.ModernGov.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Escc.ModernGov.Website.Global" Language="C#" %>
---
+++ Escc.ModernGov.Website/ModernGovModel.cs
@@ -0,0 +1,16 @@
+﻿using Escc.EastSussexGovUK.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Escc.ModernGov.Website
+{
+    /// <summary>
+    /// View model for the Modern.gov template
+    /// </summary>
+    /// <seealso cref="Escc.EastSussexGovUK.Mvc.BaseViewModel" />
+    public class ModernGovModel : BaseViewModel
+    {
+    }
+}
